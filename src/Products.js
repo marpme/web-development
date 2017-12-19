e61d137730e6360534f03012da22b5614366c9c5
Marvin Piekarek <marvinpiekarek@Marvins-MacBook-Pro.local>
2017-12-19 12:50:00 +0100

    next

--- src/Products.js
+++ src/Products.js
@@ -1,54 +1,54 @@
 import React from 'react'
 import { Table } from 'reactstrap'
-import { products } from './productList.json'
+import { products, errors } from './productList.json'
 import Product from './Product'
 import { chacheRequest } from './api/Caching'
 import { allCocktailDetails } from './api/Cocktailing'
 
 export default class Products extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cocktails: products,
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			cocktails: products,
+		}
+	}
 
-    componentDidMount() {
-        chacheRequest('cocktails', allCocktailDetails).then(state =>
-            this.setState(state)
-        )
-    }
+	componentDidMount() {
+		chacheRequest('cocktails', allCocktailDetails)
+			.then(state => this.setState(state))
+			.catch(error => this.setState({ cocktails: errors }))
+	}
 
-    render() {
-        return (
-            <div>
-                <span>
-                    <h1>Unsere Produkte:</h1>
-                    <p className="subtitle">
-                        Suchen Sie immer noch einen Drink? Hier werden sie
-                        fündig!
-                    </p>
-                </span>
-                <br />
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Preise</th>
-                            <th>Beschreibung</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.cocktails.map((product, index) => (
-                            <Product
-                                key={index}
-                                index={index + 1}
-                                {...product}
-                            />
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<span>
+					<h1>Unsere Produkte:</h1>
+					<p className="subtitle">
+						Suchen Sie immer noch einen Drink? Hier werden sie
+						fündig!
+					</p>
+				</span>
+				<br />
+				<Table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Preise</th>
+							<th>Beschreibung</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.cocktails.map((product, index) => (
+							<Product
+								key={index}
+								index={index + 1}
+								{...product}
+							/>
+						))}
+					</tbody>
+				</Table>
+			</div>
+		)
+	}
 }
--- src/SentForm.js
+++ src/SentForm.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import {
-    Card,
-    Button,
-    CardHeader,
-    CardFooter,
-    CardBody,
-    CardTitle,
-    CardText,
+	Card,
+	CardHeader,
+	CardFooter,
+	CardBody,
+	CardTitle,
+	CardText,
 } from 'reactstrap'
 
 export default ({ email, name, cocktail, message }) => (
-    <Card>
-        <CardHeader tag="h3">Deine gesendete Kontaktanfrage</CardHeader>
-        <CardBody>
-            <CardTitle>Message:</CardTitle>
-            {message}
-            <CardText />
-        </CardBody>
-        <CardFooter className="text-muted">
-            sent at {new Date().toISOString()} | Email: {email} | Name: {name}
-        </CardFooter>
-    </Card>
+	<Card>
+		<CardHeader tag="h3">Deine gesendete Kontaktanfrage</CardHeader>
+		<CardBody>
+			<CardTitle>Message:</CardTitle>
+			{message}
+			<CardText />
+		</CardBody>
+		<CardFooter className="text-muted">
+			sent at {new Date().toISOString()} | Email: {email} | Name: {name}
+		</CardFooter>
+	</Card>
 )
